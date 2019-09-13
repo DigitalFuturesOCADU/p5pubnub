3477fb94d6e587a48936dba82c98e1c72e5cc07e
Nick Puckett <nick@altnresearch.com>
2019-09-12 20:51:51 -0400

    updated examples

--- part1/09_dots/sketch.js
+++ part1/09_dots/sketch.js
@@ -1,5 +1,6 @@
 /*
-Randomizes the position/size of incoming text, but only updates on new messages.
+
+send mouse clicks
  */
 
 
@@ -11,20 +12,18 @@ let dataServer;
 let pubKey = 'pub-c-746abc06-ab6a-4450-b91f-c90e3e492917';
 let subKey = 'sub-c-f13c7c34-d4bb-11e9-8ccb-1a8b78dd2c27';
 
-
-
 //name used to sort your messages. used like a radio station. can be called anything
-let channelName = "sayStuff";
+let channelName = "clickChannel";
 
-let incomingText = ""; //variable that will hold the incoming message text
-let randPosX = 0;
-let randPosY = 0;
-let randSize = 0;
+
+let inmouseX;
+let inmouseY;
+
 
 function setup() 
 {
   
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
   background(255);
   
   
@@ -38,34 +37,22 @@ function setup()
   });
   
   //attach callbacks to the pubnub object to handle messages and connections
-  dataServer.addListener({ message: readIncoming });
+  dataServer.addListener({ message: readIncoming});
   dataServer.subscribe({channels: [channelName]});
 
-  //create the text fields for the message to be sent
-  sendText = createInput();
-  sendText.position(5,height-100);
 
-  sendButton = createButton('Post Message');
-  sendButton.position(sendText.x + sendText.width,height-100);
-  sendButton.mousePressed(sendTheMessage);
-
-
-  fill(0, 100);
 }
 
 function draw() 
 {
-    //background(255);
-    noStroke();
-    
-    textSize(randSize)
-    text(incomingText,randPosX, randPosY);
+fill(0);
+ellipse(inmouseX,inmouseY,50,50);
 
 }
 
 
 ///uses built in mouseClicked function to send the data to the pubnub server
-function sendTheMessage() {
+function mouseDragged() {
  
 
   // Send Data to the server to draw it in all other canvases
@@ -74,7 +61,8 @@ function sendTheMessage() {
       channel: channelName,
       message: 
       {
-        messageText: sendText.value()       //get the value from the text box and send it as part of the message   
+        sx: mouseX,
+        sy: mouseY 
       }
     });
 
@@ -86,9 +74,8 @@ function readIncoming(inMessage) //when new data comes in it triggers this funct
   // simple error check to match the incoming to the channelName
   if(inMessage.channel == channelName)
   {
-  incomingText = inMessage.message.messageText;
-  randPosX = random(5,width-100);
-  randPosY = random(5,height-20);
-  randSize = random(10,100);
+    console.log(inMessage.message);
+    inmouseX = inMessage.message.sx;
+    inmouseY = inMessage.message.sy;
   }
 }
